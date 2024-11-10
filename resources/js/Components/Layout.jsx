08bbb8a71e153936895b9b2a320c1c453520e159
Dom <100814585+LeoDomGit@users.noreply.github.com>
2024-11-10 21:30:57 +0700

    Update Layout.jsx

--- resources/js/Components/Layout.jsx
+++ resources/js/Components/Layout.jsx
@@ -1,4 +1,4 @@
-/* eslint-disable */
+/* eslint-disable */	
 import React from "react";
 import { Sidebar, Menu, MenuItem, useProSidebar, SubMenu } from "react-pro-sidebar";
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
